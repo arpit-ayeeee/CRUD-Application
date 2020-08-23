ab7729025899b9239e17ad83baa61a5a6b77e7cf
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2020-08-23 20:34:55 +0530

    Dropdown removed

--- src/components/Display.js
+++ src/components/Display.js
@@ -23,7 +23,7 @@ const Display = () => {
             <div className="row">
                 {
                     users.map((user) => (
-                        <div className="col-12 col-sm-6 col-md-4" key={user.id}>
+                        <div className="col-12 col-md-6 col-lg-4" key={user.id}>
                             <div className="card">
                                 <div className="card-header bg-dark">
                                     {user.title}
@@ -35,8 +35,8 @@ const Display = () => {
                                 <div className="card-footer bg-white">
                                     <Link to={`/edit/${user.id}`} className="btn btn-success btn-sm mr-1">Edit post</Link>
                                     <Link className="btn btn-danger btn-sm mr-1" onClick={() => deleteUser(user.id)}>Delete post</Link>
-                                    <Link className="btn btn-primary btn-sm mr-1">Like</Link>
-                                    <Link className="btn btn-secondary btn-sm">Dislike</Link>
+                                    <Link className="btn btn-primary btn-sm mr-1 visible-md">Like</Link>
+                                    <Link className="btn btn-secondary btn-sm visible-md">Dislike</Link>
                                 </div>
                             </div>
                         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,30 +3,28 @@ import {NavLink, Link} from 'react-router-dom';
  
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
-            <div className="container">
-                <Link to="/" className="navbar-brand"><em>CRUD</em></Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#toggle" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="toggle">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <NavLink exact to="/" className="nav-link"><span className="fa fa-feed fa-sm">Feeds</span></NavLink>
-                            {/* Used exact, so that there is no by default active link */}
-                        </li>
-                        <li className="nav-item">
-                            <NavLink exact to="/user" className="nav-link"><span className="fa fa-user fa-sm">Users</span></NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink exact to="/search" className="nav-link"><span className="fa fa-search fa-sm">Search</span></NavLink>
-                        </li>
-                    </ul>
-                </div>
-                <form className="form-inline my-2 my-lg-0">
-                    <Link to="/create"><button className="btn btn-outline-light my-2 my-sm-0">Create Post</button></Link>
-                </form>
-            </div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <a><Link to="/" className="navbar-brand"><em>CRUD</em></Link></a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <NavLink exact to="/" className="nav-link"><span className="fa fa-feed fa-sm">Feeds</span></NavLink>
+                                    {/* Used exact, so that there is no by default active link */}
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact to="/user" className="nav-link"><span className="fa fa-user fa-sm">Users</span></NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink exact to="/search" className="nav-link"><span className="fa fa-search fa-sm">Search</span></NavLink>
+                                </li>
+                            </ul>
+                    </div>
+                    <form className="form-inline  my-lg-0">
+                            <Link to="/create"><button className="btn btn-outline-light my-2 my-sm-0">Create</button></Link>
+                    </form>
         </nav>
     )
 }
